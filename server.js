5819b587b18a59c640f8fb07906027afa33a46bb
Divya Vikash <61134462+div-vik@users.noreply.github.com>
2022-10-08 21:48:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,100 @@
-import './App.css';
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Table from './components/Table/Table';
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Customers = require("./models/customerSchema");
+const app = express();
 
-function App() {
-  const [customers, setCustomers ] = useState("");
-  const [query, setQuery] = useState("");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  const search = (data)=>{
-    return data?.filter((item)=>item.firstName.toLowerCase().includes(query));
-    console.log(data);
-  };
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+).then(async (mongoose) => {
+        console.log(`connection successful`);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await axios.get("http://localhost:5000/customers-info");
-      setCustomers(data);
-      console.log("customers info", data);
-    };
-    fetchData();
-  }, []);
+        const customer1 = {
+            customerId: 1234,
+            firstName: "ramesh",
+            lastName: "malhotra",
+            orders:[
+                {
+                    orderID:880,
+                    amount:379,
+                    date:"2020-07-25T14:10:26.113Z"
+                },
+                {
+                    orderID: 881,
+                    amount: 479,
+                    date: "2020-08-25T14:10:26.113Z"
+                },
+                {
+                    orderID: 882,
+                    amount: 579,
+                    date: "2020-09-25T14:10:26.113Z"
+                }]
+        }
 
-  return (
-    <div className="App">
-      <input
-        type='text'
-        placeholder='Search'
-        className='search'
-        onChange={e=> setQuery(e.target.value)} />
-        <Table customer={search(customers.data)} />
-      
-    </div>
-  );
-}
+        const customer2 = {
+            customerId: 1235,
+            firstName: "john",
+            lastName: "doe",
+            orders:[
+                {
+                    orderID:883,
+                    amount:379,
+                    date:"2020-07-25T14:10:26.113Z"
+                },
+                {
+                    orderID: 884,
+                    amount: 479,
+                    date: "2020-08-25T14:10:26.113Z"
+                }]
+        }
 
-export default App;
+        const customer3 = {
+            customerId: 1236,
+            firstName: "mohd",
+            lastName: "islam",
+            orders:[
+                {
+                    orderID:886,
+                    amount:379,
+                    date:"2020-07-25T14:10:26.113Z"
+                },
+                {
+                    orderID: 887,
+                    amount: 479,
+                    date: "2020-08-25T14:10:26.113Z"
+                },
+                {
+                    orderID: 888,
+                    amount: 579,
+                    date: "2020-09-25T14:10:26.113Z"
+                },
+                {
+                    orderID: 889,
+                    amount: 579,
+                    date: "2020-10-25T14:10:26.113Z"
+                }
+            ]
+        }
+        // await Customers.insertMany([customer1, customer2, customer3])
+})
+.catch((err)=> console.log(err));
+
+
+app.get("/customers-info", (req, res)=> {
+    Customers.find({}, (error, customer) =>{
+        if(error) throw error
+        console.log(customer);
+        res.json(customer);
+    })
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port.");
+})
