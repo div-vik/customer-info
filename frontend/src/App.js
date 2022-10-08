09d840262ad2154db7d526817c1426f272958db4
Divya Vikash <61134462+div-vik@users.noreply.github.com>
2022-10-08 21:56:51 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import "./components/Search.css"
-import "./components/Table.css";
-import axios from 'axios';
-import Customers from './components/Customers';
+import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Table from './components/Table/Table';
 
 function App() {
-  const [customers, setCustomers] = useState([]);
-  const [search, setSearch] = useState('');
+  const [customers, setCustomers ] = useState("");
+  const [query, setQuery] = useState("");
 
-  useEffect(() => {
-    axios.get("http://localhost:5000/customers-info")
-    .then(res => {
-      setCustomers(res.data);
-      console.log(res.data);
-    })
-    .catch(error => console.log(error));
-}, []);
-
-  const handleChange = e => {
-    const searchTerm = e.currentTarget.value;
-    axios.get("/customers-info")
+  const search = (data)=>{
+    return data?.filter((item)=>item.firstName.toLowerCase().includes(query) || item.orders.date?.includes(query));
+    console.log(data);
   };
 
-  const filteredCustomers = customers.filter(customer =>
-    customer.firstName.includes(search)||customer.date.includes(search)
-  );
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await axios.get("http://localhost:5000/customers-info");
+      setCustomers(data);
+      console.log("customers info", data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
-      <div class="wrap">
-          <div class="search">
-                <input type="text" className="searchTerm" placeholder="Search" onChange={handleChange} />
-                <button type="submit" class="searchButton">
-                    <i class="fa fa-search"></i>
-                </button>
-          </div>
-      </div>
-      {filteredCustomers.map(customer => {
-        return (
-          <Customers
-            key={customer._id}
-            customerId={customer.customerId}
-            firstName={customer.firstName}
-            lastName={customer.lastName}
-            orderId={customer.D}
-            amount={customer.amount}
-            date={customer.date}
-          />
-        );
-      })}
+      <input
+        type='text'
+        placeholder='Search'
+        className='search'
+        onChange={e=> setQuery(e.target.value)} />
+        <Table customer={search(customers.data)} />
+      
     </div>
   );
 }
